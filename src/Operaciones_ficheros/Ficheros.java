2c4129a9d6a2d353e2fffe107e79f76498bcc4dc
ewpertdev <xblackcat93@gmail.com>
2024-09-19 08:53:58 +0200

    fixed

--- src/Operaciones_ficheros/Ficheros.java
+++ src/Operaciones_ficheros/Ficheros.java
@@ -1,108 +1,102 @@
-package Operaciones_ficheros;
-
+package Ficheros;
 import java.io.*;
 
-public class Ficheros {
-
-    // Metodo para quitar permisos
+public class Principal {
     public static void cambiarPermisos(File f) {
+        f.setExecutable(false);
         f.setReadable(false);
         f.setWritable(false);
-        f.setExecutable(false);
-    }
-
-
-    public static void comprobarDirectorio(File f) {
-        if(f.isDirectory()) {
-            listarFicheros(f);
-        }else{
-                if(f.isFile())
-                    System.out.println("");
-            }
-        }
-    }
-    public static void crearFichero(File f) {
 
     }
     public static void verPermisos(File f) {
-        String permisos = "";
+        String permisos="";
 
         //Permisos de lectura
-        if (f.canRead())
-            permisos = permisos + "r";
+        if(f.canRead())
+            permisos=permisos+"r";
         else
-            permisos = permisos + " - ";
+            permisos=permisos+"-";
+
         //Permisos de escritura
-        if (f.canWrite())
-            permisos = permisos + "w";
+        if(f.canWrite())
+            permisos=permisos+"w";
         else
-            permisos = permisos + " - ";
+            permisos=permisos+"-";
 
-        //Permiso de ejecucion
-        if (f.canExecute())
-            permisos = permisos + "x";
+        //Permisos de ejecución
+        if(f.canExecute())
+            permisos=permisos+"x";
         else
-            permisos = permisos + " - ";
+            permisos=permisos+"-";
 
-        // Can also use f.getAbsolutePath() instead of f.getName()
-        System.out.println("Los permisos del archivo " + f.getAbsolutePath() + " son " + permisos);
+        System.out.println("Los permisos del archivo "+f.getName()+" son: "+permisos);
 
     }
-
-public static void listarFicheros(File d) {
-    File[] ficheros = d.listFiles();
-
-    for(File f:ficheros) {
-        if(f.isFile())
-            System.out.println(f.getName() + " es un fichero ");
-        if(f.isDirectory())
-            System.out.println(f.getName() + " es un directorio ")
-
-    }
-
-    public static void main(String[] args) {
-
-        // Making directory
-        String nombreDirectorio = ".\\datos\\";
-        // Se escribe el nombre del fichero que se guarda fisicamente
-        String nombreFichero = "ficheroNuevo.txt";
-        File directorio = new File(nombreDirectorio);
-        File fichero = new File(nombreDirectorio + nombreFichero);
-
-        if (!directorio.exists()) {
-            // Crear directorio
-            if (directorio.mkdirs()) {
-                // Llamó a la función para crear fichero
-                crearFichero(fichero);
-
-
-                try {
-                    if (!fichero.exists()) {
-
-                        //Crearemos fisicamente el fichero
-                        if (fichero.createNewFile())
-                            System.out.println("Fichero creado");
-                        else
-                            System.out.println("Fichero no creado");
-                    } else
-                        System.out.println("Ya existe");
-
-                    // Llamo
-                    //Llamo a la funcion verPermisos
-                    verPermisos(fichero);
-                    cambiarPermisos(fichero);
-
-                    fichero.delete();
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                // Si el directorio no existe
-                System.out.println("El directorio no existe");
-            }
+    public static void crearFichero(File f) {
+        try {
+            if(!f.exists()) {
+                //Crearemos fisicamente el fichero
+                if(f.createNewFile())
+                    System.out.println("Fichero creado");
+                else
+                    System.out.println("Fichero no ha sido creado");
+            }else
+                System.out.println("El fichero ya existe");
 
         }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
     }
+    public static void listarFicheros(File d) {
+        File[] ficheros=d.listFiles();
+
+        for(File f:ficheros) {
+            if(f.isFile())
+                System.out.println(f.getName()+" es un fichero ");
+            if(f.isDirectory())
+                System.out.println(f.getName()+" es un directorio ");
+        }
+    }
+    public static void comprobarDirectorio(File f) {
+        if(f.isDirectory()) {
+            listarFicheros(f);
+        }else {
+            if(f.isFile())
+                System.out.println("Es un fichero no se puede recorrer");
+        }
+    }
+    public static void main(String[] args) {
+        //Se escribe el nombre del fichero que se guarda fisicamente
+        String Nombredirectorio = ".\\datos\\";
+        String Nombrefichero = "ficheroNuevo.txt";
+        File directorio = new File(Nombredirectorio);
+        File fichero = new File(Nombredirectorio+Nombrefichero);
+
+        if(!directorio.exists()) {
+            System.out.println("El directorio no existe");
+            if(directorio.mkdirs()) {
+                //Llamo a la función para crear fichero
+                crearFichero(fichero);
+                //Llamo a la función verPermisos
+				/*verPermisos(fichero);
+				cambiarPermisos(fichero);
+				verPermisos(fichero);
+
+				fichero.delete();*/
+            }
+            else {
+                System.out.println("No se ha podido crear el directorio");
+            }
+        }//Si el directorio no existe
+        else {
+            //Directorio existe
+            System.out.println("El directorio existe");
+            crearFichero(fichero);
+        }
+
+        comprobarDirectorio(directorio);
+
+    }
+
 }
