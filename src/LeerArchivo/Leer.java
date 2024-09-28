5689ff38028b048cffdb96ea50d43dfaac834b2c
ewpertdev <xblackcat93@gmail.com>
2024-09-28 17:14:45 +0200

    Leer archivo en java

---
+++ src/LeerArchivo/Leer.java
@@ -0,0 +1,26 @@
+package LeerArchivo;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Leer {
+    private final String rutaFichero = "hola.txt";
+
+    public void leerCadaCaracter() {
+        try {
+            File fichero = new File(rutaFichero);
+            Scanner teclado = new Scanner(fichero);
+
+            System.out.println("Leyendo...:");
+            while(teclado.hasNext()) {
+                String palabra = teclado.next();
+                System.out.println(palabra);
+            }
+            teclado.close();
+        } catch (Exception e) {
+            System.out.println("No existe el archivo");
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/LeerArchivo/Main.java
@@ -0,0 +1,10 @@
+package LeerArchivo;
+
+public class Main {
+    public static void main(String[] args) {
+        // creamos una instancia de la clase Leer e invocamos el metodo leerCadaCaracter
+
+        Leer leerFichero = new Leer();
+        leerFichero.leerCadaCaracter();
+    }
+}
