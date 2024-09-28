a87a72447c5b2dee2d40ece8940329418a294c41
ewpertdev <xblackcat93@gmail.com>
2024-09-28 17:43:34 +0200

    más comentarios

--- src/LeerArchivo/ReadChar.java
+++ src/LeerArchivo/ReadChar.java
@@ -7,16 +7,16 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadChar {
-    private final String rutaFichero = "hola.txt";
+    private final String rutaFichero = "hola.txt"; // ruta del archivo que queremos leer
 
     public void readCharMethod() {
         try {
-            File fichero = new File(rutaFichero);
-            FileReader fr = new FileReader(fichero);
-            BufferedReader br = new BufferedReader(fr);
+            File fichero = new File(rutaFichero); // Crear un objeto File para el fichero especificado
+            FileReader fr = new FileReader(fichero); // Preparar para leer el fichero
+            BufferedReader br = new BufferedReader(fr); // Utilizamos BufferedReader para leer de forma eficiente
 
-            System.out.println("LEYENDO CADA CARACTER...");
-            int caracter;
+            System.out.println("LEYENDO CADA CARÁCTER..."); // Printear un mensaje para indicar que la lectura empieza
+            int caracter; // Variable para almacenar el código ASCII de cada carácter
 
             // Seguir leyendo caracteres del archivo hasta que llegamos al final (indicado por -1)
             while ((caracter = br.read()) != -1) {
@@ -24,9 +24,10 @@ public class ReadChar {
                 System.out.println(letra); // Printear cada carácter sin espacio
 
             }
-            br.close();
+            br.close(); // Cerrar el BufferedReader para liberar recursos
             System.out.println(); // Printear una nueva línea después de leer
         } catch (IOException e) {
+            // Si hay un error mientras está leyendo el archivo, printeamos el stack trace para depurar
             e.printStackTrace();
         }
 
