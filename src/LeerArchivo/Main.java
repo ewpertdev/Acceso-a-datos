3a8881d59ab7a6d5b10a3e184b6efa586635c902
ewpertdev <xblackcat93@gmail.com>
2024-09-28 17:46:04 +0200

    más comentarios

--- src/LeerArchivo/Main.java
+++ src/LeerArchivo/Main.java
@@ -2,12 +2,15 @@ package LeerArchivo;
 
 public class Main {
     public static void main(String[] args) {
-        // CREAR INSTANCIA Y LUEGO INVOCAR EL METODO
-
-//        ReadEverything readEverythingFichero = new ReadEverything();
-//        readEverythingFichero.leerTodo();
-
+        // Crear instancia de las clases (que necesitemos) para acceder a sus métodos
         ReadChar readCharFichero = new ReadChar();
+        // Llamar al método para leer caracteres del fichero
         readCharFichero.readCharMethod();
+
+        // Otros métodos aquí
+        // ReadEverything readEverythingFichero = new ReadEverything();
+        // readEverythingFichero.leerTodo();
+
+
     }
 }
