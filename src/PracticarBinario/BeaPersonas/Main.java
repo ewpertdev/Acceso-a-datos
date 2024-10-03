a9d36661b16ecc5c73a1fd2ceae8f4cb22bd2e4f
ewpertdev <xblackcat93@gmail.com>
2024-10-03 09:33:09 +0200

    suffering

---
+++ src/PracticarBinario/BeaPersonas/Main.java
@@ -0,0 +1,59 @@
+package PracticarBinario.BeaPersonas;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void escribirPersonas(List<Persona> p, String ruta){
+
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(ruta)));
+            oos.writeObject(p);
+            System.out.println("Las personas han sido guardadas");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static List <Persona> leerPersonas(String ruta){
+        List <Persona> personas = new ArrayList<>();
+
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(ruta)));
+            personas = (List <Persona>)ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.getMessage();
+            e.printStackTrace();
+        }
+        return personas;
+
+
+    }
+    public static void main(String[] args){
+        String nombre ="";
+        String ruta ="ficheroPersonas.bin";
+        int edad;
+        List<Persona> personas = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce los datos de las personas");
+
+        int opcion=1;
+        while(opcion!=0) {
+            // Pedir todos los datos
+            System.out.println("Introduce el nombre");
+            nombre=sc.nextLine();
+            System.out.println("Introduce la edad");
+            edad=sc.nextInt();
+//            Persona personax= new Persona(nombre, edad);
+//            personas.add(personax);
+            personas.add(new Persona(nombre,edad));
+
+            System.out.println("Continue? (0 para salir, cualquier otro número para seguir");
+            opcion=sc.nextInt();
+        }
+        // Haremos la llamada a escribir el array en el fichero
+        escribirPersonas(personas, ruta);
+    }
+}
---
+++ src/PracticarBinario/BeaPersonas/Persona.java
@@ -0,0 +1,11 @@
+package PracticarBinario.BeaPersonas;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
